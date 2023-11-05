0e6cf63bd8460645b3300aab2d4750ed944140e2
vinniiee <vs28245@gmail.com>
2023-11-05 19:37:35 +0530

    adjust main heading color & padding in navbar

--- src/components/landing-page/Header.jsx
+++ src/components/landing-page/Header.jsx
@@ -1,4 +1,4 @@
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import Flame from "../../assets/Flame";
 import RingButton from "../../ui/RingButton";
 
@@ -19,44 +19,59 @@ export default function Header() {
         }}
       >
         <div className="absolute  uppercase text-5xl leading-none  sm:text-6xl md:text-8xl bottom-10  text-right right-10    drop-shadow-2xl">
-        <Flame
-        className=""
-          size={250}
-          initial={{  y: 100, opacity: 0 }}
-          animate={{
-            y: 20,
-            opacity: 1,
-          }}
-          transition={{
-            delay: 6,
-            duration: 2,
-          }}
-        />
+          <Flame
+            className=""
+            size={250}
+            initial={{ y: 100, opacity: 0 }}
+            animate={{
+              y: 20,
+              opacity: 1,
+            }}
+            transition={{
+              delay: 6,
+              duration: 2,
+            }}
+          />
           <motion.h1
             initial={{ y: 40, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 5.5 }}
-            className=" inline-block sm:text-midnight text-metal  md:text-white lg:text-coffee  font-primary drop-shadow-sm  "
+            className=" inline-block  text-coffee font-medium lg:font-normal lg:text-white  font-primary drop-shadow-sm"
           >
-            Take a <span className="">Break</span>.
+            Take a{" "}
+            <span className="text-white lg:text-coffee lg:font-medium">
+              Break
+            </span>
+            .
           </motion.h1>
           <br />
           <motion.h1
             initial={{ y: -40, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ delay: 6.5 }}
-            className=" inline-block text-coffee xl:text-white font-primary"
+            className=" inline-block text-white xl:font-medium xl:text-coffee font-primary"
           >
-            have some <span className="text-white xl:text-coffee">Coffee</span>.
+            have some{" "}
+            <span className="text-coffee font-medium xl:font-normal xl:text-white">
+              Coffee
+            </span>
+            .
           </motion.h1>
-          <br/>
+          <br />
           <motion.div
-          initial={{ x: -40, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ delay: 6.5,duration:2 }}
-          className=" mt-8 ml-16 "><RingButton arrowColor={'text-dark-coffee-2'} textColor={'coffee'} label={"order now"}/></motion.div>
+            initial={{ x: -40, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 6.5, duration: 2 }}
+            className=" mt-8 ml-16 "
+          >
+            <RingButton
+              arrowColor={"text-dark-coffee-2"}
+              textColor={"coffee"}
+              label={"order now"}
+            />
+          </motion.div>
         </div>
       </motion.div>
     </div>
-  )
+  );
 }
--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -22,12 +22,12 @@ export default function Navbar() {
 
   return (
     <div
-      className={`flex w-screen h-auto justify-between items-strech px-8 pl-16 sm:pl-32 sm:pr- md:px-32 -my-8  fixed    ${
+      className={`flex w-screen h-auto justify-between items-strech px-8 pt-8 pl-16 sm:pl-32 sm:pr- md:px-32 -my-8  fixed    ${
         sticky ? "top-0" : "-top-full"
       } z-50    transition-all duration-1000`}
     >
       <HeaderLogo />
-      <motion.div initial={{y:-200}} animate={{y:0}} transition={{duration:2,delay:6}} className="flex justify-center items-center sm:pt-20 space-x-2 md:space-x-4 -ml-16 ">
+      <motion.div initial={{y:-200}} animate={{y:0}} transition={{duration:2,delay:6}} className="flex justify-center items-center pt-14 sm:pt-24  space-x-2 md:space-x-4 -ml-16 ">
         <div className="w-[50px] md:w-auto" >
           <img className="w-full h-full" src="/ui/cart.svg" alt="cart" />
         </div>
