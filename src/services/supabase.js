ae6ceda3db6019025750ca314d23eaaa45c7a44d
vinniiee <vs28245@gmail.com>
2023-11-17 05:42:52 +0530

    rollback temp

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("supabase key",import.meta.env.VITE_SUPABASE_KEY);
-
-console.log("supabase url",import.meta.env.VITE_SUPABASE_URL);
 
 
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
