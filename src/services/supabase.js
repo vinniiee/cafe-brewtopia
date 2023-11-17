718738808e384809cb53149f734d15f4dd16a1ee
vinniiee <vs28245@gmail.com>
2023-11-17 05:34:37 +0530

    temp

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log(import.meta.env.VITE_SUPABASE_KEY);
+console.log("supabase key",import.meta.env.VITE_SUPABASE_KEY);
+
+console.log("supabase url",import.meta.env.VITE_SUPABASE_URL);
+
+
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
 
 export default supabase;
